833992858f655f80fd6b36d3609d1927b78c4997
page <page@magicare.me>
2018-09-22 20:37:55 +0800

    fixed:free the const str

--- src/util-inl.h
+++ src/util-inl.h
@@ -57,9 +57,7 @@ inline void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *b
 
 inline void free_write_req(uv_write_t *req) {
     write_req_t *wr = (write_req_t*) req;
-    if(wr->buf.len > 1){
-        free(wr->buf.base);
-    }
+    free(wr->buf.base);
     free(wr);
 }
 
--- src/worker.cpp
+++ src/worker.cpp
@@ -269,7 +269,7 @@ void on_new_task(uv_stream_t *server, int status) {
         uv_write_t *write_req = (uv_write_t*) malloc(sizeof(uv_write_t));
         dummy_buf = uv_buf_init("a",1);
         struct child_worker *worker = &workers[round_robin_counter];
-        uv_write2(write_req, (uv_stream_t*) &worker->pipe, &dummy_buf, 1, (uv_stream_t*) client, finish_write);
+        uv_write2(write_req, (uv_stream_t*) &worker->pipe, &dummy_buf, 1, (uv_stream_t*) client, NULL);
         round_robin_counter = (round_robin_counter + 1) % child_worker_count;
     }
     else {
